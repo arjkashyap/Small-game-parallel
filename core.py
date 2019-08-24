00ea332826c336e1a26c9c410a00d73ca82121de
Arjun <arj.kashyap123@gmail.com>
2019-08-24 15:04:43 +0530

    p2 walk and run

--- core.py
+++ core.py
@@ -9,64 +9,76 @@ pygame.init()
 W, H = 1366, 768
 
 posX, posY = W // 4, H // 2 - 140    # Player co-ordinates
-posX2, posY2 = posX, posY * 3 / 2 + 105
+posX2, posY2 = posX, posY * 3 / 2 + 105     # Player 2 co-ordinates
 
 gameDisplay = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.display.set_caption("Parallel")
-# Background Image
+
+# Background Images
 bgUp, bgDown = pygame.image.load('sprites/bgUp.jpg').convert(), pygame.image.load('sprites/bgDown.jpg').convert()
 bgUp, bgDown = pygame.transform.scale(bgUp, (W, H // 2)), pygame.transform.scale(bgDown, (W, H // 2))
+
 clock = pygame.time.Clock()
 run = True
 fps = 15
 vel = 0         # inital speed of the player
-# Player container
+
+#Player Common sprites
+idle = [pygame.image.load("sprites/player/Idle/id1-r.png").convert_alpha(),
+        pygame.image.load("sprites/player/Idle/id1-l.png").convert_alpha(),
+]
+
+run_right = [
+        pygame.image.load("sprites/player/running/r0.png").convert_alpha(),
+        pygame.image.load("sprites/player/running/r1.png").convert_alpha(),
+        pygame.image.load("sprites/player/running/r2.png").convert_alpha(),
+        pygame.image.load("sprites/player/running/r3.png").convert_alpha(),
+        pygame.image.load("sprites/player/running/r4.png").convert_alpha(),
+        pygame.image.load("sprites/player/running/r5.png").convert_alpha(),
+        pygame.image.load("sprites/player/running/r6.png").convert_alpha(),
+    ]
+
+run_left = [
+        pygame.image.load("sprites/player/running/l0.png").convert_alpha(),
+        pygame.image.load("sprites/player/running/l1.png").convert_alpha(),
+        pygame.image.load("sprites/player/running/l2.png").convert_alpha(),
+        pygame.image.load("sprites/player/running/l3.png").convert_alpha(),
+        pygame.image.load("sprites/player/running/l4.png").convert_alpha(),
+        pygame.image.load("sprites/player/running/l5.png").convert_alpha(),
+        pygame.image.load("sprites/player/running/l6.png").convert_alpha(),
+    ]
+
+jump = [
+        pygame.image.load("sprites/player/jump/jump-up-l.png").convert_alpha(),
+        pygame.image.load("sprites/player/jump/jump-fall-l.png").convert_alpha(),
+        pygame.image.load("sprites/player/jump/jump-up-r.png").convert_alpha(),
+        pygame.image.load("sprites/player/jump/jump-fall-r.png").convert_alpha(),
+    ]
 
 
+# Class for player1
 class PlayerUP(object):
 
     def __init__(self, x, y, width, height, end):
-        self.x = x
+        self.x = x              #Position x and y
         self.y = y
         self.width = width
         self.height = height
         self.isJump = False
         self.jumpCount = 10
+
         # Speed of moving
         self.end = end
         self.health = 10
         self.faceLeft = False
         self.faceRight = True
-        self.idle = [
-            pygame.image.load("sprites/player/Idle/id1-r.png").convert_alpha(),
-            pygame.image.load("sprites/player/Idle/id1-l.png").convert_alpha(),
-        ]
-        self.run_right = [
-            pygame.image.load("sprites/player/running/r0.png").convert_alpha(),
-            pygame.image.load("sprites/player/running/r1.png").convert_alpha(),
-            pygame.image.load("sprites/player/running/r2.png").convert_alpha(),
-            pygame.image.load("sprites/player/running/r3.png").convert_alpha(),
-            pygame.image.load("sprites/player/running/r4.png").convert_alpha(),
-            pygame.image.load("sprites/player/running/r5.png").convert_alpha(),
-            pygame.image.load("sprites/player/running/r6.png").convert_alpha(),
-        ]
 
-        self.run_left = [
-            pygame.image.load("sprites/player/running/l0.png").convert_alpha(),
-            pygame.image.load("sprites/player/running/l1.png").convert_alpha(),
-            pygame.image.load("sprites/player/running/l2.png").convert_alpha(),
-            pygame.image.load("sprites/player/running/l3.png").convert_alpha(),
-            pygame.image.load("sprites/player/running/l4.png").convert_alpha(),
-            pygame.image.load("sprites/player/running/l5.png").convert_alpha(),
-            pygame.image.load("sprites/player/running/l6.png").convert_alpha(),
-        ]
+        # Load sprites for player 1
+        self.idle = idle
+        self.run_right = run_right
+        self.run_left = run_left
+        self.jump = jump
 
-        self.jump = [
-            pygame.image.load("sprites/player/jump/jump-up-l.png").convert_alpha(),
-            pygame.image.load("sprites/player/jump/jump-fall-l.png").convert_alpha(),
-            pygame.image.load("sprites/player/jump/jump-up-r.png").convert_alpha(),
-            pygame.image.load("sprites/player/jump/jump-fall-r.png").convert_alpha(),
-        ]
         # Initial index value for sprite
         self.index = 0
 
@@ -94,15 +106,16 @@ class PlayerDown:
         # Speed of moving
         self.end = end
         self.health = 10
-        self.faceLeft = False
-        self.faceRight = True
-        #self.img1 = pygame.transform.rotate(self.img1, 180)
-        self.idle = [  
-        pygame.image.load("sprites/player/Idle/id1-l.png").convert_alpha(), pygame.image.load("sprites/player/Idle/id1-r.png").convert_alpha(),
-        ]
-        self.idle = [pygame.transform.rotate(img, 180) for img in self.idle]
-        self.index = 0
+        self.faceLeft = True
+        self.faceRight = False
+       
+       # Load sprites for player 2
+        self.idle = [pygame.transform.rotate(img, 180) for img in idle]                 
+        self.run_right = [pygame.transform.rotate(img, 180) for img in run_left]       # Loading and tilt images by 180 degree 
+        self.run_left = [pygame.transform.rotate(img, 180) for img in run_right]
+        self.jump = [pygame.transform.rotate(img, 180) for img in jump]
 
+        self.index = 0
     def action(self, surface, velocity):
         if self.index >= 6:
             self.index = 0
@@ -145,10 +158,21 @@ while run:
     elif p.faceLeft:
         gameDisplay.blit(p.idle[1], (p.x, p.y))
     
-
     #Motion Conrtols Player 2
+    if keys[pygame.K_RIGHT] and p2.x < W - abs(vel) - 50:
+        p2.faceRight, p2.faceLeft = False, True
+        vel = 18
+        p2.action(gameDisplay, vel)
 
-    gameDisplay.blit(p2.idle[0], (p2.x, p2.y))
+    elif keys[pygame.K_LEFT] and p2.x > 0:
+        p2.faceRight, p2.faceLeft = True, False
+        vel = -18
+        p2.action(gameDisplay, vel)
+
+    elif p2.faceRight:
+        gameDisplay.blit(p2.idle[0], (p2.x, p2.y))
+    elif p2.faceLeft:
+        gameDisplay.blit(p2.idle[1], (p2.x, p2.y))
 
     # Jump
     if not p.isJump:
